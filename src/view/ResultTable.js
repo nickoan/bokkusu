2bf51e0af6bd7d5daff056d44eafbfd2dbf149ab
Nick An <anning0322@gmail.com>
2020-10-18 11:39:55 +1100

    add result table and knex builder

---
+++ src/view/ResultTable.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Table from "react-bootstrap/Table";
+import styled from "styled-components";
+
+const TableArea = styled.div`
+ width: 100%;
+ height: calc(65.5vh);
+ overflow: scroll;
+`;
+
+const Td = styled.td`
+  min-width:50px;
+  max-width:100px;
+  overflow: scroll;
+`;
+
+const Th = styled.th`
+  min-width:80px;
+  max-width:100px;
+  overflow: scroll;
+`;
+
+const generateTable = (result) => {
+  const tHeads = result.fields.map(
+    (field) => (<Th>{field.name}</Th>)
+  );
+
+  const tBodies = result.rows.map((row) => {
+    const tmp = row.map((col) => {
+      return (<Td>
+        {col}
+      </Td>);
+    });
+    return (<tr>{tmp}</tr>);
+  });
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {tHeads}
+        </tr>
+      </thead>
+      <tbody>
+        {tBodies}
+      </tbody>
+    </Table>
+  )
+};
+
+const sample= {
+  fields: [{name: 'name1'}, {name: 'name2'}, {name: 'name3'}],
+  rows:[
+    ['123123124124124123124124125125125125141413', '456', '789'],
+    ['123', '456', '789'],
+    ['123', '456', '789'],
+  ]
+}
+
+export default React.memo(() => {
+  return (
+    <TableArea>
+      {generateTable(sample)}
+    </TableArea>
+  );
+});
--- src/view/layout.js
+++ src/view/layout.js
@@ -2,16 +2,34 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import LeftNav from "./LeftNav";
 import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ResultTable from "./ResultTable";
 
 const StyledDiv = styled.div`
+ width: 100%;
  padding: 0px;
+`;
+
+const RightSection = styled.div`
+ margin-left: 200px;
+ width: calc(100vw - 200px);
+ padding: 10px;
+`;
+
+const ScriptArea = styled.textarea`
+ width: 100%;
+ height: calc(30vh);
+ resize:none;
 `;
 
 ReactDOM.render(
   <React.StrictMode>
     <StyledDiv>
       <LeftNav />
-      hello world {process.env.NVM_DIR}
+      <RightSection>
+        <ScriptArea/>
+        <ResultTable />
+      </RightSection>
     </StyledDiv>
   </React.StrictMode>,
   document.getElementById('root')
